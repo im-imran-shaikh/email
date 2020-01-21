c893936b1df4395be88fd07962b3e8309bf7aa16
idc-120 <idc-120@idc120>
2020-01-21 15:11:56 +0530

    Frontend is created for custom email.

--- email/src/main/java/com/imran/email/controller/emailController.java
+++ email/src/main/java/com/imran/email/controller/emailController.java
@@ -2,8 +2,11 @@ package com.imran.email.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.imran.email.dto.Email;
 import com.imran.email.service.EmailService;
 
 @RestController
@@ -12,9 +15,29 @@ public class emailController
 	@Autowired
 	private EmailService emailService;
 	
+	@Autowired
+	private Email email;
+	
 	@GetMapping(path = "/sendEmail")
-	private String sendMessage()
+	private String sendMessage(@RequestParam String receipent, @RequestParam String subject, @RequestParam String body)
 	{	
+		email.setReceipent(receipent);
+		email.setSubject(subject);
+		email.setText(body);
+		
 		return emailService.sendEmail();
+	}
+	
+	@GetMapping(path = "/sendDefaultEmail")
+	private String sendDefaultEmail()
+	{
+		return emailService.sendEmail();
+	}
+	
+	@GetMapping(path = "/email")
+	private ModelAndView email()
+	{
+		ModelAndView modelAndView = new ModelAndView("email");
+		return modelAndView;
 	}
 }
---
+++ email/src/main/webapp/view/email.jsp
@@ -0,0 +1,32 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+#textboxid {
+	height: 25px;
+	font-size: 14pt;
+}
+</style>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+	<form action="/sendEmail">
+		<h2>
+			Enter receipent email address &nbsp; <input id="textboxid"
+				type="text" name="receipent"
+				, placeholder="Receipent" , style = "width : 300px", required /> <br> <br>
+			Enter subject &nbsp; <input id="textboxid" type="text" name="subject"
+				, placeholder="Subject" , style="width: 500px"
+				,  required /> <br>
+			<br> Enter Body &nbsp; <textarea  name="body"
+				row = 30, cols = 100, style="height : 100px; font-size: 14pt" required > </textarea> <br>
+			<br> 
+			<br> <input id="textboxid" type="submit" , value="send email"
+				, style="margin-left: 10%; background-color : green"  /> <input id="textboxid" type = "Reset", value = "clear" style="margin-left : 2%; background-color : red"/>
+		</h2>
+	</form>
+</body>
+</html>
